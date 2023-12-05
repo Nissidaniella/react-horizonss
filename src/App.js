4eea3135cb9e6691550116f531b7a25819afb736
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2023-12-05 23:49:44 +0000

    page2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import LandingPage from "./LandingPage";
 import { Navbar } from "./Navbar"
+import { Page2 } from "./Page2";
 
 function App() {
   return (
     <>
     <Navbar/>
     <LandingPage/>
+    <Page2/>
     </>
   );
 }
---
+++ src/Page2.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+export const Page2 = () =>{
+
+    return (
+        
+   <div className="container content-wrapper page2">
+   <div className="row">
+       <div className="col-md-6">
+           <div className="pin1"></div>
+           <div className="pin2"></div>
+           <div className="pin3"></div>
+
+           <div className="pan1"></div>
+           <div className="pan2"></div>
+           <div className="pan3"></div>
+           
+
+       </div>
+
+
+       <div className="col-md-6 ">
+           <h1> Metaverse <br></br>
+           Metaverse <span className="name">Development </span> </h1>
+
+           <p className="page26">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad alias necessitatibus iste impedit? Facilis fugiat ex enim quo nostrum tempora repellat hic explicabo. Dicta fugit vel animi voluptatum velit illum! Lorem ipsum  sit amet consectetur adipisicing elit. Fuga nam, impedit aliquam iure pariatur eaque sapiente consequatur, et est</p>
+
+           <div className="ben1">
+               <div className="dropdown">
+                   <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                       <span className="glyphicon glyphicon-sound-5-1 " aria-hidden="true"></span>
+                       <p className="p3">necessitatibus iste impedit?</p>
+                       <span></span>
+                       </button>
+                   <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+                     <li><a href="#">Action</a></li>
+                     <li><a href="#">Another action</a></li>
+                     <li><a href="#">Something else here</a></li>
+                     <li role="separator" className="divider"></li>
+                     <li><a href="#">Separated link</a></li>
+                   </ul>                                                      
+                 </div>
+
+           </div>
+
+           <div class="ben2">
+                <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle1" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <span class="glyphicon glyphicon-sound-5-1 page30" aria-hidden="true" ></span>
+                        <p className="p3">Necessitatibus iste impedit? </p>
+                       <p className="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci optio veniam, laborum amet sunt eos,
+                        <br></br>
+                         quibusdam autem perspiciatis corporis earum eius distinctio! Atque quasi fuga odit repudiandae,
+                          <br></br>
+                           placeat  
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci possimus fugiat 
+                         <br></br>
+                          quidem. Aut provident excepturi consectetur, asperiores, molestiae </p>
+                   
+                    <span class="caret page33"></span>
+                    </button>
+                    
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                    </ul>                                                      
+                  </div>
+            </div>
+
+            <div class="ben3">
+                <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle2" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <span class="glyphicon glyphicon-sound-5-1 page34" aria-hidden="true" ></span>
+                        <p className="p3">necessitatibus iste impedit?</p>
+                        <span className="caret p"></span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                    </ul>                                                      
+                  </div>
+            </div>
+
+        </div>
+
+    </div>
+   </div>
+  
+
+ 
+    )
+}
