0a4add0d5e8f49e10a5425245656c0426558e8e1
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2023-12-05 23:52:00 +0000

    redLine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LandingPage from "./LandingPage";
 import { Navbar } from "./Navbar"
 import { Page2 } from "./Page2";
+import { RedLine } from "./RedLine";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Navbar/>
     <LandingPage/>
     <Page2/>
+    <RedLine/>
     </>
   );
 }
---
+++ src/RedLine.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const RedLine = () => {
+    return (
+     
+   <div class="container red"> 
+   <div class="row">
+       <div class="col-md-12">
+          <div class="line"> </div>
+       </div>
+
+   </div>
+  </div>
+       
+    );
+};
